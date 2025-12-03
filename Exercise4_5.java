7c3a7bc60deb57857bad824b07e6e3dfda2d9d48
250541019-BERAY <250541019@firat.edu.tr>
2025-12-03 18:13:54 +0300

    Update Exercise4_5.java

--- Exercise4_5.java
+++ Exercise4_5.java
@@ -1,27 +1,24 @@
-public class Exercise4_5 {
-
-    public static void zoop(String fred, int bob) {
-        System.out.println(fred);              // 2, 8   -> zoop ilk çağrıda 2. adımda; ikinci çağrıda 8. adımda
-        if (bob == 5) {
-            ping("not ");                      // 3      -> sadece ilk zoop çağrısında çalışır
-        } else {
-            System.out.println("!");          // 9      -> sadece ikinci zoop çağrısında çalışır
-        }
-    }
-
-    public static void main(String[] args) {
-        int bizz = 5;                          // (değişken ataması, yürütülür ama sıraya genelde dahil edilmeyen setup)
-        int buzz = 2;
-        zoop("just for", bizz);               // 1
-        clink(2 * buzz);                      // 5  (önce 2*buzz hesaplanır, sonra clink çağrılır)
-    }
-
-    public static void clink(int fork) {
-        System.out.print("It’s ");            // 6
-        zoop("breakfast ", fork);             // 7
-    }
-
-    public static void ping(String strangStrung) {
-        System.out.println("any " + strangStrung + "more "); // 4
-    }
-}
+------------------------------
+| ping                       |
+| strangStrung = "not "      |
+------------------------------
+| zoop                       |
+| fred = "just for"          |
+| bob  = 5                   |
+------------------------------
+| main                       |
+| bizz = 5                   |
+| buzz = 2                   |
+------------------------------
+------------------------------
+| zoop                       |
+| fred = "breakfast "        |
+| bob  = 4                   |
+------------------------------
+| clink                      |
+| fork = 4                   |
+------------------------------
+| main                       |
+| bizz = 5                   |
+| buzz = 2                   |
+------------------------------
