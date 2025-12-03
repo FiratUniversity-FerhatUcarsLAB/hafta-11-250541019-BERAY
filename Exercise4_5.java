84213d307f5925b4a65a8d960db33ddc650e50fb
250541019-BERAY <250541019@firat.edu.tr>
2025-12-03 21:23:39 +0300

    Update Exercise4_5.java

--- Exercise4_5.java
+++ Exercise4_5.java
@@ -1,24 +1,54 @@
-------------------------------
-| ping                       |
-| strangStrung = "not "      |
-------------------------------
-| zoop                       |
-| fred = "just for"          |
-| bob  = 5                   |
-------------------------------
-| main                       |
-| bizz = 5                   |
-| buzz = 2                   |
-------------------------------
-------------------------------
-| zoop                       |
-| fred = "breakfast "        |
-| bob  = 4                   |
-------------------------------
-| clink                      |
-| fork = 4                   |
-------------------------------
-| main                       |
-| bizz = 5                   |
-| buzz = 2                   |
-------------------------------
+public class Exercise4_5 {
+
+    public static void zoop(String fred, int bob) {
+        System.out.println(fred);
+        if (bob == 5) {
+            ping("not ");
+        } else {
+            System.out.println("!");
+        }
+    }
+
+    public static void main(String[] args) {
+        int bizz = 5;
+        int buzz = 2;
+        zoop("just for", bizz);
+        clink(2 * buzz);
+    }
+
+    public static void clink(int fork) {
+        System.out.print("It's ");
+        zoop("breakfast ", fork);
+    }
+
+    public static void ping(String strangStrung) {
+        System.out.println("any " + strangStrung + "more ");
+    }
+}
+
+
+Çıktı
+ just for 
+any  not  more
+It 's   breakfast
+!
+
+  Stack (En üst = aktif metod)
+---------------------------------
+| zoop(" breakfast ", bob=4)    |
+|   - fred = " breakfast "       |
+|   - bob = 4                    |
+|   - Aktif satır: println(fred)|
+---------------------------------
+| clink(fork=4)                  |
+|   - fork = 4                   |
+|   - Aktif satır: zoop(...)     |
+---------------------------------
+| main(args, bizz=5, buzz=2)     |
+|   - bizz = 5                   |
+|   - buzz = 2                   |
+|   - Aktif satır: clink(2*buzz)|
+---------------------------------
+
+
+  
