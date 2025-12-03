c72efc41ac3684dd91b5b8f9759f068f713d5dfe
250541019-BERAY <250541019@firat.edu.tr>
2025-12-03 17:33:39 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,21 @@
 public class Exercise4_1 {
-
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        // Amerikan format: Monday, July 22, 2019
+        System.out.println(day + ", " + month + " " + date + ", " + year); // yürütme sırasında çalışır
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+        // Avrupa format: 22 July 2019, Monday
+        System.out.println(date + " " + month + " " + year + ", " + day); // yürütme sırasında çalışır
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        String day = "Monday";    //  (örnek veri)
+        String month = "July";
+        int date = 22;
+        int year = 2019;
+
+        printAmerican(day, date, month, year); // 1 -> çağrı: Amerikan formatı yazdırılır
+        printEuropean(day, date, month, year); // 2 -> çağrı: Avrupa formatı yazdırılır
     }
 }
